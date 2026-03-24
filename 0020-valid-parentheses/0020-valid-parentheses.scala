a28144e0febfd9a080cf515a966b20390ccbc267
mohdalimuddin1001 <mohdalim.uddin1001@gmail.com>
2026-03-24 23:47:18 +0530

    Time: 37 ms (70.54%), Space: 58.9 MB (47.29%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.scala
+++ 0020-valid-parentheses/0020-valid-parentheses.scala
@@ -1,23 +1,23 @@
 object Solution {
     def isValid(s: String): Boolean = {
     val sArray = s.toCharArray
-    val length = sArray.length
+    val sLength = s.length
+
     val openingChar = Map('(' -> 1, '[' -> 2, '{' -> 3)
     val closingChar = Map(')' -> 1, ']' -> 2, '}' -> 3)
 
-    def helper(index: Int, s: Array[Int]): Array[Int] = {
-      if (index >= length) s
-      else if (closingChar.keys.exists(_ == sArray(index)) && s.nonEmpty && s.last == closingChar.getOrElse(sArray(index), 0)) helper(index + 1, s.init)
-      else if (closingChar.keys.exists(_ == sArray(index)) && s.nonEmpty && s.last != closingChar.getOrElse(sArray(index), 0))
-        Array(' ')
-      else if (openingChar.keys.exists(_ == sArray(index))) {
-        val value = openingChar.getOrElse(sArray(index), 0)
-        helper(index + 1, s :+ value)
+    def helper(index: Int, isValidList: List[Int]): Option[List[Int]] = {
+      if (index >= sLength) Some(isValidList)
+      else if (openingChar.contains(sArray(index))) helper(index + 1, openingChar(sArray(index)) :: isValidList)
+      else if (closingChar.contains(sArray(index))) {
+        isValidList match {
+          case h :: tail if h == closingChar(sArray(index)) => helper(index + 1, tail)
+          case _ => None
+        }
       }
-      else {
-        Array(' ')
-      }
+      else None
     }
-    helper(0, Array.emptyIntArray).isEmpty
+
+    helper(0, Nil).exists(_.isEmpty)
     }
 }
