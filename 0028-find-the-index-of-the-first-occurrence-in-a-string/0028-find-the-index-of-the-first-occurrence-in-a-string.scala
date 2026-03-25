3ae42aa1d9df6b11b5f1c61b78817d35eed7d696
mohdalimuddin1001 <mohdalim.uddin1001@gmail.com>
2026-03-25 22:40:23 +0530

    Time: 1 ms (65.12%), Space: 56.8 MB (86.05%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.scala
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.scala
@@ -5,10 +5,10 @@ object Solution {
     val haystackLength = haystackArray.length
     val needleLength = needleArray.length
     def helper(haystackIndex: Int, needleIndex: Int, index: Int, start: Int): Int = {
-      if (needleIndex == needleLength) index
+      if (needleIndex == needleLength) start
       else if (haystackIndex >= haystackLength || needleIndex > haystackIndex) -1
       else if (needleArray(needleIndex) == haystackArray(haystackIndex)) helper(haystackIndex + 1, needleIndex + 1, index, start)
-      else helper(start + 1, 0, start + 1, start + 1)
+      else helper(start + 1, 0, haystackIndex, start + 1)
     }
     helper(0, 0, 0, 0)
     }
