1b3fad646e2f6e349678073c2300518316c210e0
mohdalimuddin1001 <mohdalim.uddin1001@gmail.com>
2026-03-29 21:32:08 +0530

    Time: 24 ms (72.97%), Space: 56.9 MB (78.38%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.scala
+++ 0058-length-of-last-word/0058-length-of-last-word.scala
@@ -1,11 +1,22 @@
 object Solution {
     def lengthOfLastWord(s: String): Int = {
-        val wordsArray = s.split("\\s+")
-        val lastWord = wordsArray.last
-        def helper(word: String, count: Int): Int = {
-        if (word.isEmpty) count
-        else helper(word.tail, count + 1)
+    val sReverse = s.reverse
+    val sLength = sReverse.length
+    def helper(s: String, count: Int, index: Int, countAchieved: Boolean): Int = {
+        if (countAchieved) count
+        else {
+            if (sLength <= 1) helper(s, count + 1, index, true)
+            else if (index == sLength && count != 0) helper(s, count, index, true)
+            else if (sLength <= 2) {
+            if (s(index) == ' ' && count != 0) helper(s, count,  index, true)
+            else if (s(index) != ' ' && s(index).isValidChar && count != 0) helper(s, count,  + 1, false)
+            else helper(s, count + 1, index, true)
+            }
+            else if (s(index) == ' ' && count == 0) helper(s, count, index + 1, false)
+            else if (s(index) == ' ' && count != 0) helper(s, count, index, true)
+            else helper(s, count + 1, index + 1, false)
         }
-        helper(lastWord, 0)
+        }
+        helper(sReverse, 0, 0, false)
     }
 }
