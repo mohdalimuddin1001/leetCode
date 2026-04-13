7f051046cd590cca11b2151ca49b8b2b189f91d7
mohdalimuddin1001 <mohdalim.uddin1001@gmail.com>
2026-04-13 21:47:55 +0530

    Time: 3 ms (90.63%), Space: 60 MB (56.25%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.scala
+++ 0283-move-zeroes/0283-move-zeroes.scala
@@ -1,17 +1,15 @@
 object Solution {
     def moveZeroes(nums: Array[Int]): Unit = {
         val nLength = nums.length
-        def helper(i: Int, j: Int, nArray: Array[Int]): Array[Int] = {
-        if (j >= nLength) nArray
-        else {
-            if (nArray(j) != 0) {
-            val temp = nArray(i)
-            nArray(i) = nArray(j)
-            nArray(j) = temp
-            helper(i + 1, j + 1, nArray)
-            } else helper(i, j + 1, nArray)
+        def helper(i: Int, j: Int): Unit = {
+        if (j >= nLength) return 
+        if (nums(j) != 0) {
+            val temp = nums(i)
+            nums(i) = nums(j)
+            nums(j) = temp
+            helper(i + 1, j + 1)
+        } else helper(i, j + 1)
         }
-        }
-        helper(0, 0, nums)
+        helper(0, 0)
     }
 }
